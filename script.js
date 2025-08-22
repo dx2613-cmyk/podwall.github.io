194041d702807561ce5905532f7f54b4ee5ed4f6
dx2613-cmyk <dx.2613@gmail.com>
2025-08-22 11:23:45 +0700

    script.js

--- script.js
+++ script.js
@@ -250,7 +250,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 {
                   icon: 'M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z',
                   title: 'График работы',
-                  text: 'Пн–Сб: 09:00–29:00<br>Вс: выходной'
+                  text: 'Пн–Сб: 09:00–20:00<br>Вс: выходной'
                 },
                 {
                   icon: 'M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z',
@@ -377,7 +377,7 @@ document.addEventListener('DOMContentLoaded', function() {
             <h4 class="text-lg font-semibold mb-4">Контакты</h4>
             <div class="space-y-2 text-gray-400">
               <p>г. Новосибирск, ул. Театральная 4/1</p>
-              <p>Пн–Сб: 09:00–29:00</p>
+              <p>Пн–Сб: 09:00–20:00</p>
               <p>Вс: выходной</p>
               <p>+7 952 909-69-09</p>
               <p>ceis.ooo@yandex.ru</p>
